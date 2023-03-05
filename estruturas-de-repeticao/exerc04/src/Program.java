e0eb7f1fa6cb1389009e8292bff21dc869f96575
Wilker Freire <wilkerf8@gmail.com>
2023-03-04 22:29:11 -0300

    Correção: comentando codigo com enunciado do exercicio

--- estruturas-de-repeticao/exerc04/src/Program.java
+++ estruturas-de-repeticao/exerc04/src/Program.java
@@ -1,6 +1,13 @@
 public class Program {
     public static void main(String[] args) {
 
+        /*
+        Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento
+        de 3% e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Faça um programa
+        que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a
+        população do país B, mantidas as taxas de crescimento.
+         */
+
         int anos = 0;
         double A = 80000;
         double B = 200000;
